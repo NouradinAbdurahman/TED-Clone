b6b2a189d789572209330c6c8264dc241b260698
NOURADDIN ⭐️ <70664023+NouradinAbdurahman@users.noreply.github.com>
2022-07-26 00:55:24 +0300

    initial commit

---
+++ JS/main.js
@@ -0,0 +1,12 @@
+console.log("TED Clone!");
+
+/*
+    // NOTE:
+            
+    - Please use before every begining of section comment like:
+        < start "name of the section">
+
+        < end "name of the section">
+
+    *feel free to remove this comment. 
+*/
